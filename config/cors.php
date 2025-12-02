332d814a1ccf90b723d7b0fa2ad141e21623f8b7
Ricardo Sanchez <sanchezveraricardo@gmail.com>
2025-12-02 10:27:24 -0500

    Allow Netlify domain in CORS

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://training-calendar-managment.netlify.app/'
     ],
 
     'allowed_origins_patterns' => [],
