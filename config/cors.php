d6051404b98b28fe5fe6d278b15e2dfae3fb769f
Ricardo Sanchez <sanchezveraricardo@gmail.com>
2025-12-02 12:13:53 -0500

    Add admin seeder for productionnn

--- config/cors.php
+++ config/cors.php
@@ -3,28 +3,26 @@
 return [
 
     'paths' => [
-        'api/*',
-        'sanctum/csrf-cookie',
-        'login',
-        'logout',
-    ],
+    'api/*',
+    'login',
+    'logout',
+    'sanctum/csrf-cookie'
+],
 
-    'allowed_methods' => ['*'],
+'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://training-calendar-managment.netlify.app', // tu frontend en Netlify
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-    ],
+'allowed_origins' => [
+    'https://training-calendar-managment.netlify.app',
+    'http://localhost:5173'
+],
 
-    'allowed_origins_patterns' => [],
+'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+'exposed_headers' => [],
 
-    'max_age' => 0,
+'max_age' => 0,
 
-    // Importante para que funcione con credenciales (cookies de Sanctum)
-    'supports_credentials' => true,
+'supports_credentials' => true,
 ];
