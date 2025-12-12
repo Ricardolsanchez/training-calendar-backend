15eb0d61f2225ac32d3870b212280dac220ed64a
Ricardo Sanchez <sanchezveraricardo@gmail.com>
2025-12-12 12:25:01 -0500

    Update backend new

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use App\Http\Controllers\BookingController;
 
 // Puedes dejarlo vacío o comentar todo de momento,
 // porque este archivo no se está cargando.
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/health/db', function () {
+    DB::select('select 1');
+    return ['ok' => true];
+});
